c7df5ce442b4142e3c77bbfd993847d8abfb9514
theArcticOcean <wei_yang1994@163.com>
2022-05-20 17:48:02 +0800

    update filenameReplace.sh

--- filenameReplace.sh
+++ filenameReplace.sh
@@ -1,14 +1,19 @@
-#! /bin/bash
-
-var=$(ls |tr " " "\n")
-# default IFS is '\n'
-beforeList=($var)
-len=${#beforeList[@]}
-afterList=($(ls |awk '{ print $1 }' |sed 's/netmusicapp/XMLABookApp/g' |tr " " "\n"))
+#! /usr/bin/bash
+if [ $# -lt 2 ]; then
+        echo "useage: script <oldName> <newName>"
+        exit 1
+fi
+oldName=$1
+newName=$2
+var=$(ls)
+beforeList=($var) # to array
+len=${#beforeList[@]} # array length
+afterList=($(ls |sed s/${oldName}/${newName}/g))
+#echo ${afterList[@]}
 
 i=0
 while [ $i -lt $len ]; do
-	echo ${beforeList[$i]}" --> "${afterList[$i]}
-	mv ${beforeList[$i]} ${afterList[$i]} 2>/dev/null
-	i=$(expr $i + 1)
+        echo ${beforeList[$i]}" --> "${afterList[$i]}
+        mv ${beforeList[$i]} ${afterList[$i]} 2>/dev/null
+        i=$(expr $i + 1)
 done
